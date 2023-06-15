05aac883fb60de0c9fd14767a07844ba4f3493c9
Douglas Viana De Macedo <douglasvianademacedo2002@gmail.com>
2023-06-15 18:05:49 -0300

    testando interação com vs code

--- src/ProjetoLivro.java
+++ src/ProjetoLivro.java
@@ -1,5 +1,5 @@
-public class App {
+public class ProjetoLivro {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("testando 1  2 3"); 
     }
 }
