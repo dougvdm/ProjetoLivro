b405e604568097df99381598a3dd703d19d9c4f0
Douglas Viana De Macedo <douglasvianademacedo2002@gmail.com>
2023-06-15 23:54:09 -0300

    primeira escolha

--- src/ProjetoLivro.java
+++ src/ProjetoLivro.java
@@ -4,11 +4,11 @@ public class ProjetoLivro {
     public static void main(String[] args) throws Exception {
         System.out.println("Capítulo 1\n\n");
         System.out.println("A história de Deco");
-        escolher1();
+        escolher();
         
     }
 
-    private static void escolher1() {
+    private static void escolher() {
         Scanner input = new Scanner(System.in); 
         System.out.println(
     "Deco, também conhecido como o último romantico do mundo é um rapaz que está" + 
@@ -19,13 +19,20 @@ public class ProjetoLivro {
         System.out.println("nao deve se declarar\n");
         String escolha = input.nextLine();
         if(escolha.equalsIgnoreCase("deve se declarar")){
-            declarou();
+            System.out.println("JP acaba descobrindo das segundas intenções de Deco e ");
         }
-        else if(escolha.equalsIgnoreCase("nao deve se declarar"));
-            System.out.println("FIM");
-    }
+        else if(escolha.equalsIgnoreCase("nao deve se declarar")){
+                 naodeclarou();
+        }
+        else{
+            System.out.println("resposta incorreta, tente novamente\n\n");
+              escolher();
+        }
+            
+            
+    }   
 
-    private static void declarou() {
-        System.out.println("Deco decidiu se declarar para Laura porém, o que ele não contava era");
+    private static void naodeclarou() {
+
     }
 }
